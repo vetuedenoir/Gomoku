27fb2c2d60a1dabe252311b52a740ebcf444ec96
Killian SCORDEL <kscordel@f6r11s4.clusters.42paris.fr>
2026-04-02 18:03:59 +0200

    refacto/ui/restructure file, create interface.hpp et cpp

---
+++ include/MenuePage.hpp
@@ -0,0 +1,12 @@
+#ifndef MENUEPAGE_HPP
+# define MENUEPAGE_HPP
+
+#include "interface.hpp"
+
+class MenuePage {
+	private:
+		
+}
+
+
+#endif
---
+++ include/interface.hpp
@@ -0,0 +1,69 @@
+#ifndef INTERFACE_HPP
+# define INTERFACE_HPP
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <string>
+
+
+
+constexpr unsigned int WIN_W = 800;
+constexpr unsigned int WIN_H = 600;
+
+// Colors
+inline const sf::Color BG        (18,  18,  32);
+inline const sf::Color GOLD      (220, 175,  80);
+inline const sf::Color WHITE     (230, 230, 230);
+inline const sf::Color DIM       (100, 100, 120);
+
+// ── helpers ─────────────────────────────────────────────────────────────────
+
+void centerOrigin(sf::Text &t);
+
+sf::Text makeText(const std::string &str, const sf::Font &font,
+						  unsigned int size, sf::Color color);
+
+void drawGrid(sf::RenderWindow &window);
+
+// ── menu item ─────────────────────────────────────────────────────────────────
+
+struct Item
+{
+	sf::Text        label;
+	sf::RectangleShape bg;
+
+	Item(const std::string &str, const sf::Font &font, float x, float y)
+		: label(makeText(str, font, 38, WHITE))
+	{
+		label.setPosition(x, y);
+
+		bg.setSize(sf::Vector2f(260.f, 58.f));
+		bg.setOrigin(130.f, 29.f);
+		bg.setPosition(x, y);
+		bg.setFillColor(sf::Color::Transparent);
+		bg.setOutlineThickness(2.f);
+		bg.setOutlineColor(DIM);
+	}
+
+	bool contains(sf::Vector2f pt) const
+	{
+		return bg.getGlobalBounds().contains(pt);
+	}
+
+	void setHovered(bool hovered)
+	{
+		label.setFillColor(hovered ? GOLD : WHITE);
+		bg.setOutlineColor(hovered ? GOLD : DIM);
+	}
+
+	void draw(sf::RenderWindow &w) const
+	{
+		w.draw(bg);
+		w.draw(label);
+	}
+};
+
+
+
+
+#endif
