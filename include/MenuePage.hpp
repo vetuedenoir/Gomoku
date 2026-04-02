1ef99527c5b9c559ab5a346e17cf8efa27938f27
Killian SCORDEL <kscordel@f6r11s4.clusters.42paris.fr>
2026-04-02 18:25:05 +0200

    feature/ui/prototype de la classe MenuePage

--- include/MenuePage.hpp
+++ include/MenuePage.hpp
@@ -2,10 +2,14 @@
 # define MENUEPAGE_HPP
 
 #include "interface.hpp"
+#include <unordered_map>
 
 class MenuePage {
 	private:
-		
+		std::unordered_map<std::string, sf::Text> text_map;
+		std::unordered_map<std::string, sf::RectangleShape> rectangle_map;
+		std::unordered_map<std::string, Item> item_map;
+
 }
 
 
