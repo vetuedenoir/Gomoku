97b4b674858e345a3a0effdb705b1fac8701410c
kscordel <killianscordel@gmail.com>
2026-04-30 20:07:21 +0200

    feature: ajout d'une table de lookup pour les 4 en ligne avec une case vide au milieu

--- include/bitboard/pattern.hpp
+++ include/bitboard/pattern.hpp
@@ -2,6 +2,7 @@
 #define PATTERN_HPP
 
 #include "bitboard.hpp"
+#include "string.h"
 
 #define	MAX_WINNING_MASK	1020
 #define	MAX_FOUR_MASK	1120
@@ -30,6 +31,19 @@ typedef struct {
 	uint8_t padding[4];		// 4 bytes de padding pour aligner à 232 bytes
 } t_MaskList4;
 
+typedef struct {
+	uint64_t	patterns[3][6];  // 3 patterns × 6 uint64_t = 144 bytes
+	int			hole_pos[3];	// 12 bytes
+} t_PatternGroup; // 156 bytes total
+
+
+typedef struct {
+	t_PatternGroup	masks[4]; // position relative. 624 bytes
+	uint32_t		count;	// 4 bytes
+	uint8_t			padding[4];	// 4 bytes de padding pour aligner à 632 bytes
+} t_MaskList_Groupe4;  // 632 bytes total
+
+
 int	isWin_ultra(t_MaskList5 lookup_table5[361][4], const bitboard19& board,
 	const int x, const int y);
 int	is_Open_4(t_MaskList4 lookup_table4[361][4], const bitboard19 &boardA, const bitboard19 &boardB,
@@ -46,3 +60,185 @@ void	add_mask_to_lookup4(t_MaskList4 lookup_table4[361][4], bitboard19 all_masks
 void	test_bitboard(const GameBoard& board, int x, int y); // Fonction de test pour vérifier les patterns sur le bitboard
 
 #endif // PATTERN_HPP
+
+
+// liste des patternes
+	// 1. alignement de 5 pierres (gagne)
+	// 2. alignement de 4 pierres partiellement ouvert (open four)
+	// 3. alignement de 4 pierres completement ouvert (open four)
+	// 4. alignement de 4 pierres troue (broken four)
+	// [ ] [x] [x] [x] [x] [ ]  -> open four
+	// [A] [x] [x] [x] [x] [ ]  -> partiel four
+	// [ ] [x] [x] [x] [x] [A]  -> partiel four
+
+	// [X] [ ] [x] [x] [x]  -> partiel four
+	// [X] [X] [ ] [x] [x]  -> partiel four
+	// [X] [X] [x] [ ] [x]  -> partiel four
+
+	// [X] [ ] [X] [X] [x] [ ] [x]  -> open four inutile
+	// [X] [ ] [x] [x] [x] [ ] [ ]  -> proto open four inutile
+	// [ ] [ ] [X] [X] [x] [ ] [x]  -> proto open four inutile
+
+	// 5. alignement de 3 pierres partiellement ouvert (open three)
+	// 6. alignement de 3 pierres completement ouvert (open three)
+	// 7. alignement de 3 pierres troue (broken three)
+	// [ ] [x] [x] [x] [ ]  -> open three
+	// [ ] [ ] [x] [x] [x] [ ] [ ] -> open three ???
+	// [A] [ ] [x] [x] [x] [ ] [ ] -> open three ???
+	// [ ] [ ] [x] [x] [x] [ ] [A] -> open three ???
+	// [A] [ ] [x] [x] [x] [ ] [A] -> open three ???
+
+
+	// [A] [x] [x] [x] [ ] [ ]-> partiel three
+	// [ ] [ ] [x] [x] [x] [A]-> partiel three
+
+	// [ ] [X] [ ] [x] [x] [ ]  -> partiel three ???
+	// [ ] [x] [x] [ ] [x] [ ]  -> partiel three ???
+
+
+	// 8 Les paterns en crois. legal si creer grace a capture.
+	// [ ] [x] [ ]
+	// [x] [x] [x]
+	// [ ] [x] [ ]
+
+	// [ ] [x] [ ]
+	// [x] [ ] [x]
+	// [ ] [x] [ ]
+
+	// 9. Les paterns en T// liste des patternes
+	// 1. alignement de 5 pierres (gagne)
+	// 2. alignement de 4 pierres partiellement ouvert (open four)
+	// 3. alignement de 4 pierres completement ouvert (open four)
+	// 4. alignement de 4 pierres troue (broken four)
+	// [ ] [x] [x] [x] [x] [ ]  -> open four
+	// [A] [x] [x] [x] [x] [ ]  -> partiel four
+	// [ ] [x] [x] [x] [x] [A]  -> partiel four
+
+	// [X] [ ] [x] [x] [x]  -> partiel four
+	// [X] [X] [ ] [x] [x]  -> partiel four
+	// [X] [X] [x] [ ] [x]  -> partiel four
+
+	// [X] [ ] [X] [X] [x] [ ] [x]  -> open four inutile
+	// [X] [ ] [x] [x] [x] [ ] [ ]  -> proto open four inutile
+	// [ ] [ ] [X] [X] [x] [ ] [x]  -> proto open four inutile
+
+	// 5. alignement de 3 pierres partiellement ouvert (open three)
+	// 6. alignement de 3 pierres completement ouvert (open three)
+	// 7. alignement de 3 pierres troue (broken three)
+	// [ ] [x] [x] [x] [ ]  -> open three
+	// [ ] [ ] [x] [x] [x] [ ] [ ] -> open three ???
+	// [A] [ ] [x] [x] [x] [ ] [ ] -> open three ???
+	// [ ] [ ] [x] [x] [x] [ ] [A] -> open three ???
+	// [A] [ ] [x] [x] [x] [ ] [A] -> open three ???
+
+
+	// [A] [x] [x] [x] [ ] [ ]-> partiel three
+	// [ ] [ ] [x] [x] [x] [A]-> partiel three
+
+	// [ ] [X] [ ] [x] [x] [ ]  -> partiel three ???
+	// [ ] [x] [x] [ ] [x] [ ]  -> partiel three ???
+
+
+	// 8 Les paterns en crois. legal si creer grace a capture.
+	// [ ] [x] [ ]
+	// [x] [x] [x]
+	// [ ] [x] [ ]
+
+	// [ ] [x] [ ]
+	// [x] [ ] [x]
+	// [ ] [x] [ ]
+
+	// 9. Les paterns en T
+	// [ ] [x] [ ]
+	// [x] [x] [x]
+	// [ ] [ ] [ ]
+	// 10. Les paterns en L
+	// [x] [ ]
+	// [x] [ ]
+	// [x] [ ]
+	// [ ] [x]
+
+
+	// double open three, legal seulement si les deux open three ne se croisent pas
+	// ou si il est creer grace a une capture.
+	// [A] [A] [ ]  [A] [A] [X]
+	// [A] [X] [ ]  [A] [X] [ ]
+	// [X] [A] [X]  [X] [A] [X]
+	// [A] [A] [X]  [A] [A] [X]
+
+
+	// T 3 2 : techniquement pas un double open tree
+	// [X] [ ] [X]
+	// [ ] [X] [ ]
+	// [ ] [X] [ ]
+	// [ ] [X] [ ]
+	// [ ] [A] [ ]
+
+	// the T 4 3, potentiellement legal d'apres le sujet.
+	//         [ ]
+	// [ ] [X] [X] [X] [] si encore un espace a gauche ou a droite, victoire assurer.
+	//         [X]
+	//         [X]
+	//         [X]
+	//         [A]
+
+	// proto double three a demi ouvert
+	// [X] [ ] [X]
+	//     [X]
+	// [ ] [X] [ ]
+	// [ ] [A] [ ]
+
+	// double three a demi ouvert
+	//         [ ]
+	//         [ ]
+	// [ ] [X] [X] [X] [ ] si encore un espace a gauche ou a droite, victoire assurer.
+	//         [X] 			si c'est a moi de jouer.
+	//         [X]
+	//         [A]
+
+	// [ ] [x] [ ]
+	// [x] [x] [x]
+	// [ ] [ ] [ ]
+	// 10. Les paterns en L
+	// [x] [ ]
+	// [x] [ ]
+	// [x] [ ]
+	// [ ] [x]
+
+
+	// double open three, legal seulement si les deux open three ne se croisent pas
+	// ou si il est creer grace a une capture.
+	// [A] [A] [ ]  [A] [A] [X]
+	// [A] [X] [ ]  [A] [X] [ ]
+	// [X] [A] [X]  [X] [A] [X]
+	// [A] [A] [X]  [A] [A] [X]
+
+
+	// T 3 2 : techniquement pas un double open tree
+	// [X] [ ] [X]
+	// [ ] [X] [ ]
+	// [ ] [X] [ ]
+	// [ ] [X] [ ]
+	// [ ] [A] [ ]
+
+	// the T 4 3, potentiellement legal d'apres le sujet.
+	//         [ ]
+	// [ ] [X] [X] [X] [] si encore un espace a gauche ou a droite, victoire assurer.
+	//         [X]
+	//         [X]
+	//         [X]
+	//         [A]
+
+	// proto double three a demi ouvert
+	// [X] [ ] [X]
+	//     [X]
+	// [ ] [X] [ ]
+	// [ ] [A] [ ]
+
+	// double three a demi ouvert
+	//         [ ]
+	//         [ ]
+	// [ ] [X] [X] [X] [ ] si encore un espace a gauche ou a droite, victoire assurer.
+	//         [X] 			si c'est a moi de jouer.
+	//         [X]
+	//         [A]
